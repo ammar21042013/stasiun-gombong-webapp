2080cec02520e5afed9b9f9c374d60c1500f3385
ammar21042013 <ammar.21042013@gmail.com>
2026-08-15 20:31:17 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIpcXpptLulX3Rpi-vQHrAhPQ_eGd-Cpc",
+  authDomain: "stasiun-gombong-web-app.firebaseapp.com",
+  projectId: "stasiun-gombong-web-app",
+  storageBucket: "stasiun-gombong-web-app.firebasestorage.app",
+  messagingSenderId: "851029796299",
+  appId: "1:851029796299:web:24a34f32d8ea83931ec24e",
+  measurementId: "G-XFCDL1MTXW"
+};
